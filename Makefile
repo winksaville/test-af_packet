60239efcad576599a5f9a1b91b66fd5f8e87f5bb
Wink Saville <wink@saville.com>
2016-06-08 10:24:07 -0700

    Add creating a socket
    Added a run and run-no-sudo target, using AF_PACKET requires elevated
    permissions on most systems. Not sure if its required for circleci.

--- Makefile
+++ Makefile
@@ -3,5 +3,11 @@
 test-af_packet : test-af_packet.c
 	gcc -std=c11 $< -o $@
 
+run : test-af_packet
+	sudo ./test-af_packet
+
+run-no-sudo : test-af_packet
+	./test-af_packet
+
 clean :
 	rm test-af_packet
--- test-af_packet.c
+++ test-af_packet.c
@@ -1,8 +1,32 @@
+/**
+ * This software is released into the public domain.
+ *
+ * Based on Graham Shaw's [microhowto.info](http://www.microhowto.info/howto/send_an_arbitrary_ethernet_frame_using_an_af_packet_socket_in_c.html)
+ * and RFC's such as [rfc1180](https://tools.ietf.org/html/rfc1180)
+ */
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/if_ether.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
   printf("test-af_packet: argc=%d\n", argc);
   for (int i = 0; i < argc; i++) {
     printf("argv[%d]=%s\n", i, argv[i]);
+  }
+
+  int fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_ARP));
+  if (fd < 0) {
+    printf("%s\n", strerror(errno));
+  } else {
+    printf("Success: fd=%d\n", fd);
+  }
+
+  if (fd >= 0) {
+    close(fd);
   }
 }
